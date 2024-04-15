f328adef77d691b72136ba22b57176d91903694f
SabaXdev <sabakatamadze@gmail.com>
2024-04-15 10:31:21 +0400

    Third commit

--- market/models.py
+++ market/models.py
@@ -1,19 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-# Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=255, verbose_name='Book Name')
-    page_count = models.IntegerField(null=True, blank=True, verbose_name='Page Count')
-    category = models.CharField(max_length=255, verbose_name='Category')
-    author_name = models.CharField(max_length=255, verbose_name='Author')
-    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Price')
-    image = models.ImageField(upload_to='books/', null=True, blank=True, verbose_name='Image')
-    description = models.TextField(default='', verbose_name='Description')
+    name = models.CharField(max_length=255, verbose_name=_('Book Name'), help_text=_('Enter the name of the book'))
+    page_count = models.IntegerField(null=True, blank=True, verbose_name=_('Page Count'), help_text=_('Enter the number of pages in the book'))
+    category = models.CharField(max_length=255, verbose_name=_('Category'), help_text=_('Enter the category of the book'))
+    author_name = models.CharField(max_length=255, verbose_name=_('Author'), help_text=_('Enter the name of the author'))
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name=_('Price'), help_text=_('Enter the price of the book'))
+    image = models.ImageField(upload_to='books/', null=True, blank=True, verbose_name=_('Image'), help_text=_('Upload an image of the book cover'))
+    description = models.TextField(default='', verbose_name=_('Description'), help_text=_('Enter a description of the book'))
 
     def __str__(self):
         return f"{self.name} by {self.author_name}"
 
     class Meta:
-        verbose_name = "Book"
-        verbose_name_plural = "Books"
+        verbose_name = _("Book")
+        verbose_name_plural = _("Books")

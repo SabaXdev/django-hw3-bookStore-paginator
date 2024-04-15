10323f2e3dc71df887190e1ea7995996227b93d0
SabaXdev <sabakatamadze@gmail.com>
2024-04-15 10:22:34 +0400

    Second commit

--- market/models.py
+++ market/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=255)
-    page_count = models.IntegerField()
-    category = models.CharField(max_length=255)
-    author_name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='books/', null=True, blank=True)
-    description = models.TextField(default='')
+    name = models.CharField(max_length=255, verbose_name='Book Name')
+    page_count = models.IntegerField(null=True, blank=True, verbose_name='Page Count')
+    category = models.CharField(max_length=255, verbose_name='Category')
+    author_name = models.CharField(max_length=255, verbose_name='Author')
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Price')
+    image = models.ImageField(upload_to='books/', null=True, blank=True, verbose_name='Image')
+    description = models.TextField(default='', verbose_name='Description')
 
     def __str__(self):
         return f"{self.name} by {self.author_name}"
